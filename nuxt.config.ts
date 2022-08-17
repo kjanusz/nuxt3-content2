f1ae5ec269bfc1155f5b9c4e4a769b256f4d70a8
kjanusz <kjanusz@users.noreply.github.com>
2022-08-17 14:18:31 +0200

    add router base configuration for github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
   // static HTML generation (SSG)
   ssr: true,
   target: "static",
+  // add router base configuration for github pages
+  router: {
+    base:'/nuxt3-content2/',
+  },
   modules: ['@nuxt/content'],
   content: {
     // https://content.nuxtjs.org/api/configuration
