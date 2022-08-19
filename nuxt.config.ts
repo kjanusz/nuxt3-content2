559ba895bab8299045f24a609bcef4bcba6b213d
kjanusz <kjanusz@users.noreply.github.com>
2022-08-19 14:52:15 +0200

    modify defineNuxtConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // static HTML generation (SSG)
-  ssr: true,
-  target: "static",
+  // 1) static hosting SSG - npm generate, preview
+  ssr: true, // ssr:true is default
+  // 2) static hosting SPA - npm build
+  //ssr: false,
+  // 3) SSR - npm build
+  //ssr: true,
   // modify baseURL for github pages
   app: {
     baseURL:'/nuxt3-content2/',
